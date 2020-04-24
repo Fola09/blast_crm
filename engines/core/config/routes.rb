56fc272a0de19aaad46545bdb8a987ddd821e510
fola09 <folaolayiwola.fo@gmail.com>
2020-04-24 01:27:09 +0100

    Setting up the Admin Panel

--- engines/core/config/routes.rb
+++ engines/core/config/routes.rb
@@ -1,4 +1,8 @@
 Blast::Core::Engine.routes.draw do
-  devise_for :users, class_name: "Blast::User", module: :devise
+    devise_for :users, class_name: "Blast::User", module: :devise
     root to: 'dashboard#index'
+    namespace :admin do
+      get '/' => 'admin#index'
+      resources :users, only: :index
+    end
 end
